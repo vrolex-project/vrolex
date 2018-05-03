0104e9cea56a65baf0e4020c6aef665106895ceb
Lun <lun1109@hotmail.com>
2018-05-03 15:14:51 +0800

    update build

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 set -e
 srcdir="$(dirname $0)"
-echo "$srcdir"
 cd "$srcdir"
-ls
 if [ -z ${LIBTOOLIZE} ] && GLIBTOOLIZE="`which glibtoolize 2>/dev/null`"; then
   LIBTOOLIZE="${GLIBTOOLIZE}"
   export LIBTOOLIZE
 fi
+autoreconf --version
 autoreconf --install --force --warnings=all
